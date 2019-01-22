3127c5b7a9b666d0f2819c1538929f5bcc82724e
pker247 <legendaryphill@hotmail.com>
2019-01-22 06:01:20 -0500

    Initial Setup of Project
    Created project made minor GUI changes, and began setting up database controls.

---
+++ MESB/app/src/main/java/mqp/mesb/MainActivity.java
@@ -0,0 +1,61 @@
+package mqp.mesb;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_peripherals:
+                    mTextMessage.setText("Current Peripherals Connected:");
+                    getPeripherals();
+                    return true;
+                case R.id.navigation_data:
+                    mTextMessage.setText("Prior Run Data:");
+                    getData();
+                    return true;
+                case R.id.navigation_settings:
+                    mTextMessage.setText("Settings:");
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    public void getPeripherals(){
+
+    }
+
+    public void getData(){
+
+    }
+
+}
